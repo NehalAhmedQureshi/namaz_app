92aa86c314264e04e6f1eb32f73927a25be65e57
Nehal Ahmed Qureshi <m.nehalq125@gmail.com>
2024-04-21 01:31:37 +0500

    enable prompt

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+let main = 'https://api.aladhan.com/v1/calendarByAddress/2024/4?address='
+let city = prompt('Enter city name to see prayer times!')
+
+let concat = main + city
+console.log(concat);
+
 function api(){
-    fetch("https://api.aladhan.com/v1/calendarByAddress/2024/4?address=Pakistan")
+    fetch(concat)
     .then( res => res.json())
-    .then ( data =>  console.log(data.data[10].timings))
+    .then(data => console.log(data.data[21].timings))
     // namaz timings => data.data[10].timings
 }
 
